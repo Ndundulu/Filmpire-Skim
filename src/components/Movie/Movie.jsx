d002ad7d3d2e4163a0943feee4f3bb180e6352c1
Kitheka <oulxxtg@gmail.com>
2025-03-09 16:49:20 +0300

    solved movie problem of not rendering the data fetched from the tmdb

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -4,11 +4,11 @@ import {Link} from 'react-router-dom';
 import { TgTitle, StyleGrid } from './style';
 
 
-export const Movie = (movie, i) => {
+export const Movie = ({movie, i}) => {
     console.log(movie, i)
   return (
     <StyleGrid item xs={12} sm={6} md={4} lg={3} xl={2} >
-    <TgTitle variant='h5'>{movie.TgTitle}</TgTitle>
+    <TgTitle variant='h5'>{movie.title}</TgTitle>
     </StyleGrid>
   )
 };
